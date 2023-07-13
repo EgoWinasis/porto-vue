8d3541d8c79c0793a8c408e1c71b14573c1dd039
Ego Winasis <62259546+EgoWinasis@users.noreply.github.com>
2023-07-14 00:14:14 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,10 @@
+//this code use to github-pages
 // module.exports = {
 //     publicPath: process.env.NODE_ENV === 'production'
 //      ? './porto-vue/'
 //      : './'
 //     }
+
 
 module.exports = {
     publicPath: '/'
